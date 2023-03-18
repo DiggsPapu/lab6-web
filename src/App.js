9723932a4b98840c591e92900de41f4afc7d47e5
DiggsPapu <alo20172@uvg.edu.gt>
2023-03-18 07:11:59 -0600

    background set

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from "react";
 import "./App.css";
 const App = () => {
   return (
-    <div>
-      <h1 className='heading__one'>React application!!!</h1>
+    <div style={{
+        background: `url("https://static.wikia.nocookie.net/4ef840a8-8212-48e9-8173-bed2d9501858") no-repeat center fixed`,
+        width: '1280px',
+        height: '630px',
+        display: 'grid',
+        gridColumn: 'repeat(4,100px)',
+        gridRow: 'repeat(10,100px)',
+        border: '2px solid yellow',
+        backgroundSize: 'contain'
+    }}>
+ 
     </div>
   );
 };
